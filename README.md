1223841084019906eae5e9de8123aa5390c33eec
Leo Benkel <leo.benkel@gmail.com>
2023-11-24 17:06:25 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# GPT Terminal
+
+This repository, named "GPT Terminal," is a tool that allows you to embed ChatGPT, a language model developed by OpenAI, into the Terminal. With this tool, you can interact with ChatGPT directly from your command line interface, enabling you to have conversational interactions with the model without the need for a separate application or interface. It provides a convenient way to access the capabilities of ChatGPT within your Terminal environment.This repository, named "GPT Terminal," is a tool that allows you to embed ChatGPT, a language model developed by OpenAI, into the Terminal. With this tool, you can interact with ChatGPT directly from your command line interface, enabling you to have conversational interactions with the model without the need for a separate application or interface. It provides a convenient way to access the capabilities of ChatGPT within your Terminal environment.
+
+## How to use?
+
+### Install
+
+Add this at the end of your bash profile file:
+
+```bash
+function get_current_time {
+    date '+%Y-%m-%d-%H-%M-%S'
+}
+
+if [[ "$INSIDE_SCRIPT" == "true" ]]; then
+    echo "Running inside 'script'"
+else
+    ( export INSIDE_SCRIPT=true && \
+      export FILE_SCRIPT_LOG="/tmp/script_log_$(get_current_time).log" && \ 
+      echo "Starting script with file ${FILE_SCRIPT_LOG}" && \
+      script $FILE_SCRIPT_LOG )
+fi
+```
+
+Now, each time you start a bash session, its log and history will be saved in `/tmp/....log`.
+
+Then run ` ln "/path/to/repo/GPT-terminal/gpt-terminal.py" "/usr/local/bin/??" `
+
+Now you can just use 
+
+```bash
+$> ?? ask a question
+```
+
+in your terminal to get chatGPT's help.
+
+![Alt Text](Screenshot.png)
---
+++ gpt-terminal.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+from openai import OpenAI
+import os
+import sys
+
+# check that OPENAI_API_KEY is defined:
+if not os.environ.get("OPENAI_API_KEY"):
+    raise Exception("OPENAI_API_KEY is not defined")
+
+# check that FILE_SCRIPT_LOG is defined:
+if not os.environ.get("FILE_SCRIPT_LOG"):
+    raise Exception("FILE_SCRIPT_LOG is not defined")
+
+def get_completion(log_history, prompt, model="gpt-3.5-turbo"):
+    openai = OpenAI(
+        api_key=os.environ.get("OPENAI_API_KEY"),
+    )
+
+    messages = [
+        {"role": "system", "content": """
+As an expert in CLI, dev, bash and all dev related fields, you can review terminal logs, provide insights and suggest commands if needed.
+You will review the previous script log context, before answering.
+
+You will then answer the user in two possible ways, either with one simple command to achieve the desire outcome or information on what is happening.
+When the answer is one single command, only return the command with no explanation. If needed, the user will ask for more information.
+        """},
+        {
+            "role":"system",
+            "content": f"""
+Current date is {os.popen("date").read()}
+
+This is the log history:
+```
+{log_history}
+```
+        """},
+        {"role": "user", "content": prompt}
+        ]
+
+    response = openai.chat.completions.create(
+        model=model,
+        messages=messages,
+    )
+    return response.choices[0].message.content
+
+def get_log_history():
+    # get the last 1000 non-blank lines from the file located at the path described by the env: FILE_SCRIPT_LOG
+    return os.popen("grep -v '^$' " + os.environ.get("FILE_SCRIPT_LOG") + " | tail -n 20").read()
+
+
+# Main:
+if __name__ == "__main__":
+    log_history = get_log_history()
+    prompt = " ".join(sys.argv[1:])
+    print(get_completion(log_history, prompt))
